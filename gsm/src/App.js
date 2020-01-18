fe6576e83feadcc5da91c6358781b35bae59ed78
pinghsu520 <pinghsu520@gmail.com>
2020-01-18 08:33:12 -0700

    template

---
+++ gsm/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import  Posts  from './components/';
+
+const App = () => {
+  return (
+    <div>
+        HI
+        <Posts />
+
+    </div>
+  );
+};
+
+export default App;
---
+++ gsm/src/components/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class Posts extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Postss
+				</h1>
+			</div>
+		)
+	}
+}
+
+export default Posts;
+
