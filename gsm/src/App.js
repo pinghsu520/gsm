518393595be16d8d72f60b7b0723615dfc00e604
pinghsu520 <pinghsu520@gmail.com>
2020-01-18 09:07:14 -0700

    menu

--- gsm/src/App.js
+++ gsm/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import  Posts  from './components/';
+import  Menu  from './components/Menu';
 
 const App = () => {
   return (
     <div>
         HI
         <Posts />
-
+        <Menu />
     </div>
   );
 };
---
+++ gsm/src/components/Menu.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+
+class Menu extends React.Component {
+	render() {
+		return (
+	<div>
+
+		<div class="ui menu">
+		  <div class="header item">
+		    Community and Social Impact
+		  </div>
+		  <a class="item">
+		    Events
+		  </a>
+		  <a class="item">
+		    Sponsors and forms
+		  </a>
+		  <a class="item">
+		    Press HQ
+		  </a>
+		  <a class="item">
+		    e-News
+		  </a>
+		</div>
+
+	</div>
+		)
+	}
+}
+
+export default Menu;
+
