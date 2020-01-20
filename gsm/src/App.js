79eb2fc454c3c1e38fb2cb2457f3fd1663f048a2
pinghsu520 <pinghsu520@gmail.com>
2020-01-20 12:33:51 -0700

    push changes

--- gsm/src/App.js
+++ gsm/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import  Posts  from './components/';
 import  Menu  from './components/Menu';
-
+import SlideShow from './components/slideShow';
 const App = () => {
   return (
     <div>
         HI
         <Posts />
         <Menu />
+        <SlideShow />
     </div>
   );
 };
---
+++ gsm/src/slideShow.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+class SlideShow extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>
+					Slideshow class
+				</h1>
+			</div>
+		)
+	}
+}
+
+export default SlideShow;
+
