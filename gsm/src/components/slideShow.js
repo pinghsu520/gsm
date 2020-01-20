103be3b10a98a0ed05dff5cd985cbf722be74b76
Andy Le <andybrvt@gmail.com>
2020-01-20 10:53:07 -0700

    push changes

--- gsm/src/components/slideShow.js
+++ gsm/src/components/slideShow.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import Carousel from 'react-bootstrap/Carousel'
-import Button from 'react-bootstrap/Button'
+import Carousel from 'react-bootstrap/Carousel';
 
 class SlideShow extends React.Component {
 
@@ -10,7 +9,48 @@ class SlideShow extends React.Component {
 				<h1>
 					This is start of slideshow
 				</h1>
-				<Button variant ="primary"> Primary</Button>
+			<Carousel>
+				<Carousel.Item>
+						<img 
+							className="d-block w-100"
+							src ="./images/img_mountains_wide.jpg"
+							alt="First slide"
+						/>
+						<Carousel.Caption>
+							<h3>First slide label</h3>
+							<p>Nulla vitae elit</p>
+						</Carousel.Caption>
+				</Carousel.Item>
+
+				<Carousel.Item>
+						<img 
+							className="d-block w-100"
+							src ="./images/img_mountains_wide.jpg"
+							alt="Third slide"
+						/>
+						
+						<Carousel.Caption>
+							<h3>Second slide label</h3>
+							<p> lorearafdljfadsf</p>
+						</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+						<img
+							className ="d-block w-100"
+							src ="./images/img_mountains_wide.jpg"
+							alt="Third slide"
+						/>
+
+					<Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>;ajf;dajsfafds</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+
+
+
+
 			</div>
 		)
 	}
