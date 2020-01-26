d790da63f236c42d8274942684b35636845eec9b
farrellt1 <42357024+farrellt1@users.noreply.github.com>
2020-01-26 13:37:28 -0700

    PlzWork

---
+++ build/precache-manifest.8836a5d014a94f1d77169d9418999a63.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b8c93215924f169bdd75f010d65cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1ac0801a22373d809919",
+    "url": "/static/css/2.a9250713.chunk.css"
+  },
+  {
+    "revision": "c90ffd42e3975c087144",
+    "url": "/static/css/main.cf5788d3.chunk.css"
+  },
+  {
+    "revision": "1ac0801a22373d809919",
+    "url": "/static/js/2.2915c8d4.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.2915c8d4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c90ffd42e3975c087144",
+    "url": "/static/js/main.4fe077de.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "8cfc12c06084d5a959fcbd620c8aa890",
+    "url": "/static/media/NatureLake.8cfc12c0.jpg"
+  },
+  {
+    "revision": "79a9052d0abb7d5cbc8c123cc5dfece5",
+    "url": "/static/media/img_fjords_wide.79a9052d.jpg"
+  },
+  {
+    "revision": "f7f3ce3fd039702a3b46fcbcff155466",
+    "url": "/static/media/img_mountains_wide.f7f3ce3f.jpg"
+  },
+  {
+    "revision": "d73dd0b67391a113150eb5661f8ff00d",
+    "url": "/static/media/img_nature_wide.d73dd0b6.jpg"
+  },
+  {
+    "revision": "65c542e938cc5a435a69a1f57186487a",
+    "url": "/static/media/natureFlower.65c542e9.jpg"
+  },
+  {
+    "revision": "da4153fde84c7c779e765756c7a70fd7",
+    "url": "/static/media/redFlowers.da4153fd.jpg"
+  }
+]);
